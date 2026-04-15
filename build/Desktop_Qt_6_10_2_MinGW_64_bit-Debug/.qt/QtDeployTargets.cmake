6569ad7a62bad786670db78a777a8a4ca853f582
Lyn3303 <3553478686@qq.com>
2026-04-15 21:54:06 +0800

    Remove build directory and add .gitignore

--- build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_spacecraftsimulator_FILE C:/Users/80621/Desktop/Database/web/project_qt/spacecraftsimulator/build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/spacecraftsimulator.exe)
-set(__QT_DEPLOY_TARGET_spacecraftsimulator_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_spacecraftsimulator_RUNTIME_DLLS D:/download/Qt/6.10.2/mingw_64/bin/Qt6Widgets.dll;D:/download/Qt/6.10.2/mingw_64/bin/Qt6Gui.dll;D:/download/Qt/6.10.2/mingw_64/bin/Qt6Core.dll)
